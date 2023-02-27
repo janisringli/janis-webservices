74a47702c47383a0d8c66d57882720ac98f5aa48
Janis Ringli <janis.ringli@gmail.com>
2023-02-27 22:15:47 +0100

    add header component and begin with basic styling

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div class="header-wrapper">
+      <div className="header-content">
+        <a className="header-item" href="#section1">
+          Section 2
+        </a>
+        <a className="header-item" href="#section2">
+          Section 2
+        </a>
+        <a className="header-item" href="#section3">
+          Section 2
+        </a>
+        <a className="header-item" href="#section4">
+          Section 2
+        </a>
+        <a className="header-item" href="#section5">
+          Section 2
+        </a>
+        <a className="header-item" href="#section6">
+          Section 2
+        </a>
+      </div>
+    </div>
+  );
+}
+export default Header;
