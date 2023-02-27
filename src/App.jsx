a7e71c5095d21c183721c6d5f49a96831189af66
Janis Ringli <janis.ringli@gmail.com>
2023-02-27 22:16:13 +0100

    add function and smooth scrolling to header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import Header from "./components/Header/Header.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header></Header>
+      <div id="section1">
+        <a href="#section2">Scroll</a>
+      </div>
+      <div id="section2"></div>
+      <div id="section3"></div>
+      <div id="section4"></div>
+      <div id="section5"></div>
+      <div id="section6"></div>
+    </div>
+  );
 }
 
 export default App;
