8be30b77fbcff4579dd2ac69571c7c5617361907
Janis Ringli <janis.ringli@gmail.com>
2023-03-04 17:46:12 +0100

    finish header style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,27 @@
 import "./Header.css";
+import logo from "../../assets/img/logo.png";
 
 function Header() {
   return (
     <div class="header-wrapper">
       <div className="header-content">
-        <a className="header-item" href="#section1">
-          Section 2
-        </a>
-        <a className="header-item" href="#section2">
-          Section 2
-        </a>
-        <a className="header-item" href="#section3">
-          Section 2
-        </a>
-        <a className="header-item" href="#section4">
-          Section 2
-        </a>
-        <a className="header-item" href="#section5">
-          Section 2
-        </a>
-        <a className="header-item" href="#section6">
-          Section 2
-        </a>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="nav">
+          <a className="nav-item" href="#section1">
+            <div className="current">About Me</div>
+          </a>
+          <a className="nav-item" href="#section2">
+            <div>Services</div>
+          </a>
+          <a className="nav-item" href="#section3">
+            <div>Models</div>
+          </a>
+          <a className="nav-item" href="#section4">
+            <div>Contact</div>
+          </a>
+        </div>
       </div>
     </div>
   );
